1447a7b1769a7f47d13cd17a0b0b78accd6d456f
whm <1183561690@qq.com>
2020-01-15 15:04:55 +0800

    eee

--- app/Http/Controllers/Common.php
+++ app/Http/Controllers/Common.php
@@ -3,21 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Cache;
 class Common extends Controller
 {
     const appid="wxdc85626268e1bafd";
     const appsecret="9f01b584b02bdf4c3c5121c07ff36feb";
     //获取Access_Token
     public static function Access_Token(){
-        $token=Redis::get('Access_Token');
+        $token=Cache::get('Access_Token');
         // Redis::del('Access_Token');die;  //清除Access_Token
         if(empty($token)) {
             $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" . self::appid . "&secret=" . self::appsecret;
             $res = Common::curlGet($url);
             $data = json_decode($res, true);
             $token=$data['access_token'];
-            Redis::set('Access_Token',$token,7200 );
+            Cache::put('Access_Token',$token,7200 );
         }
         return $token;
     }
